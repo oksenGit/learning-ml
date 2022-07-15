be785f62a5ceeae3c0b2610c0b649b7fa8076e76
Omar <omar.k.elgarhy@gmail.com>
2022-07-15 14:52:08 +0200

    python lists

---
+++ python/basics/lists.py
@@ -0,0 +1,38 @@
+a = ["apple"]
+
+b = ["banana"]
+
+print(a+b)
+print("#############################################")
+
+
+c = ["apple", "banana", "cherry", "durian", "elderberry"]
+print(c, len(c))
+
+print(c[0:3])
+
+print(c[3:])
+
+print(c[:3])
+
+print(c[-3:])
+
+print(c[-3:-1])
+print("#############################################")
+
+
+d = ("apple", "banana", "cherry", "durian", "elderberry")
+print(d, len(d))
+print("#############################################")
+
+
+e = {"apple", "banana", "cherry", "durian", "elderberry",
+
+     "apple", "banana", "cherry", "durian", "elderberry"}
+print(e, len(e))
+print("#############################################")
+
+dictionary = {"red": ["apple", "strawberry"],
+              "yellow": "banana", "green": "cherry"}
+print(dictionary)
+print("#############################################")
