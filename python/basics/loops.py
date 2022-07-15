11ad628ed9b8f73818a1fe95cabe1e51e09f2ec6
Omar <omar.k.elgarhy@gmail.com>
2022-07-15 15:11:09 +0200

    python loops

---
+++ python/basics/loops.py
@@ -0,0 +1,28 @@
+#while
+i = 1
+while i < 10:
+     print(i)
+     i += 1
+print("#############################################")
+#for
+for i in range(1, 10):
+     print(i)
+print("#############################################")
+
+#for with else
+for i in range(1, 10):
+     print(i)
+else:
+     print("i is not in the range")
+print("#############################################")
+
+#for in list
+friends = ["John", "Bob", "Mary"]
+for friend in friends:
+     print(friend)
+print("#############################################")
+
+#for in string
+for char in "abcdefghijklmnopqrstuvwxyz":
+     print(char)
+print("#############################################")
